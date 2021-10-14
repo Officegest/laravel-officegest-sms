418e115cf16450ef45eee5df29869b7fb3186f7f
Stephane Soares <stephane.soares@officegest.com>
2021-10-14 17:10:44 +0100

    Remove wrong files

--- src/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use OfficegestSms\Classes\OfficegestSms;
-
-public function dummyfunction(){
-    $sender = new OfficegestSms();
-    $sender->send('+351.123456789', 'Your text message');
-}
